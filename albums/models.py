64876293d520be3576671134d8db8be72a6ed1af
Tudor1703 <tudormirca90@gmail.com>
2025-09-23 18:38:20 +0300

    Improvements to albums/models.py 2.0 (task #17)

--- albums/models.py
+++ albums/models.py
@@ -2,30 +2,35 @@ from django.db import models
 from django.conf import settings
 import uuid
 
+from django.db import models
+from django.conf import settings
+import uuid
+
 class Album(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    share_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    share_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, null=False)
 
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,     
         related_name="albums"     
     )
+
+    emails = models.ManyToManyField(
+        "AlbumEmail", related_name="albums", blank=True
+    )
+
     def __str__(self):
         return self.title
 
 
 class AlbumEmail(models.Model):
-    album = models.ForeignKey(
-        "Album", on_delete=models.CASCADE, related_name="album_emails"
-        )
-    email = models.EmailField() 
+    email = models.EmailField(unique=True)
 
     def __str__(self):
-        return f"{self.email} (Album: {self.album.title})"
-
+        return self.email
 
 class AlbumPhoto(models.Model):
     photo = models.ImageField(upload_to="album_photos/")
