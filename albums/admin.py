2bd0d5b18997e64d8fb74896f543f72397e804d2
Tudor1703 <tudormirca90@gmail.com>
2025-10-05 20:44:21 +0300

    Fixed bugs and imrovments in albums/admin.py (task #25)

--- albums/admin.py
+++ albums/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import Album, AlbumPhoto, AlbumEmail
 
-# Inline pentru fotografii
 class AlbumPhotoInline(admin.TabularInline):
     model = AlbumPhoto
     extra = 1
     readonly_fields = ("uploaded_at",)
 
-# Inline pentru participanți (email)
 class AlbumEmailInline(admin.TabularInline):
     model = Album.emails.through
     extra = 1
@@ -17,22 +15,21 @@ class AlbumAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created_at", "photo_count", "participant_count")
     search_fields = ("title", "user__email")
     inlines = [AlbumPhotoInline, AlbumEmailInline]
+    exclude = ("emails",)
 
     readonly_fields = ("share_token",)
 
-    # Statistici
+    @admin.display(description="Photos")
     def photo_count(self, obj):
         return obj.photos.count()
-    photo_count.short_description = "Photos"
 
+    @admin.display(description="PhoParticipantstos")
     def participant_count(self, obj):
         return obj.emails.count()
-    participant_count.short_description = "Participants"
 
-    # Delete albums direct din listă
     actions = ["delete_selected_albums"]
 
-    @admin.action(description="Șterge albumele selectate")
+    @admin.action(description="Delete selected albums")
     def delete_selected_albums(self, request, queryset):
         queryset.delete()
 
