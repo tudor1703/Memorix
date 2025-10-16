6397d6af7e5321bfd1008dc0cb141a6c8689a613
Tudor1703 <tudormirca90@gmail.com>
2025-10-16 08:48:00 +0300

    Send email test (task #34)

--- albums/urls.py
+++ albums/urls.py
@@ -5,4 +5,5 @@ app_name = 'albums'
 
 urlpatterns = [
     path('<uuid:share_token>/', views.album_view, name='album_view'),
+    path('send-email/', views.send_emails_view, name='send_email'),
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib import messages
+
+from memorix.settings import EMAIL_HOST_USER
 from .models import Album
+from django.http import HttpResponse
+from django.core.mail import send_mail
 
 
 def send_emails_view(request, album_id):
-    album = get_object_or_404(Album, id=album_id)
-    recipients = [e.email for e in album.emails.all()]
-    full_url = request.build_absolute_uri(album.get_absolute_url())
-
-    messages.success(
-        request,
-        f"Simulated: emails marked as sent to {len(recipients)} participant(s)."
-    )
-
-    change_url = reverse("admin:albums_album_change", args=[album_id])
-    return redirect(f"{change_url}?emails_sent=1")
-
+    if request.method == 'POST':
+        album = Album.objects.get(id=album_id)
+        send_mail(
+            subject="Test Email",
+            message="This is a test email from Memorix.",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=["mircatudor06@gmail.com"],
+        )
+        return HttpResponse("Email sent successfully!")
+    return render(request, "albums/send_email.html")
 
 def album_view(request, share_token):
     album = get_object_or_404(Album, share_token=share_token)
